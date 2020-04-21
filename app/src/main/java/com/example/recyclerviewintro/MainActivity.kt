f3dcee42545e077ecf003eb36c9eec4ba0868029
Magnus <51696488+Magnusahl@users.noreply.github.com>
2020-04-21 14:06:39 +0200

    rec lektion

---
+++ app/src/main/java/com/example/recyclerviewintro/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.recyclerviewintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    var persons = listOf<Person>(Person("Magnus", 34), Person("Ellen", 3), Person("Therese", 32))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = PersonRecycleAdapter(this, persons)
+
+        recyclerView.adapter = adapter
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/recyclerviewintro/Person.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerviewintro
+
+class Person(var name: String, var age: Int) {
+
+
+}
